bc63e8592a660cb5fe16df64fef1c8b4bd782fd1
assiri <assiri@assiris-MacBook-Pro.local>
2019-04-01 10:31:01 +0300

    Updates

---
+++ precache-manifest.0762f8bad197448ea26df74dd6b5da8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d877c966472757057da6b24de5ed127b",
+    "url": "/qaisgroup/static/media/sp.d877c966.svg"
+  },
+  {
+    "revision": "6d3a1944c0d57a97a01b",
+    "url": "/qaisgroup/static/js/runtime~main.90399d09.js"
+  },
+  {
+    "revision": "d14a4d837af4ac6e4fd5",
+    "url": "/qaisgroup/static/js/main.4309ee36.chunk.js"
+  },
+  {
+    "revision": "6afed568379cf9ef0185",
+    "url": "/qaisgroup/static/js/2.800cc483.chunk.js"
+  },
+  {
+    "revision": "6afed568379cf9ef0185",
+    "url": "/qaisgroup/static/css/2.7c880d99.chunk.css"
+  },
+  {
+    "revision": "53710f4dc3ea710b9694255343dd4d1d",
+    "url": "/qaisgroup/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34a41831c90d1fd2aba043f8e1056987.js"
+  "/qaisgroup/precache-manifest.0762f8bad197448ea26df74dd6b5da8b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/qaisgroup/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
